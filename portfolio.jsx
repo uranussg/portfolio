94d85937cd721918c64b4bb7b74cf606b0a54bbb
uranussg <uranussg@gmail.com>
2020-04-13 16:49:24 -0700

    change

--- portfolio.jsx
+++ portfolio.jsx
@@ -7,12 +7,25 @@ document.addEventListener('DOMContentLoaded', () => {
         const y = window.scrollY
         // console.log(vh)
         nav.forEach((el, idx) => {
-            console.log(Math.floor((y + 300 + 16 * idx)/vh))
+            // console.log(Math.floor((y + 300 + 16 * idx)/vh))
             el.classList.remove('black')
             if (Math.floor((y + 300 + 16 * (idx + 0.5))/vh) == 2) {
             el.classList.add('black')
         }
         })
+
+        const about = document.getElementsByClassName('about')[0]
+        const education = document.getElementsByClassName('education')[0]
+        about.style.opacity = '0'
+        education.style.opacity = '0'
+        console.log(y/vh)
+        if ( y/vh > 0.8 && y/vh < 1.2)
+        {
+        about.style.opacity = '1'
+        education.style.opacity = '1'
+        education.transform ='translate(137%, 160%)'
+
+        }
 
       
     })
@@ -23,4 +36,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     }
+
+    window.addEventListener
 })
